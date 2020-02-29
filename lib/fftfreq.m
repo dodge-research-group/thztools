6e744793b12ed83024b140af8f64e3d754a27ec9
Steve Dodge <jsdodge@sfu.ca>
2020-02-29 13:44:21 -0800

    Revised FFTFREQ to include Nyquist frequency among positive frequencies

--- lib/fftfreq.m
+++ lib/fftfreq.m
@@ -22,10 +22,17 @@ validateattributes(ts,{'numeric'},{'scalar','positive'})
 
 fs = 1/(ts*N);
 
-% Use IFFTSHIFT to shift zero frequency from center to first position.
-kcyclic = ifftshift((0:N-1)-floor(N/2));
+% Use CIRCSHIFT to shift zero frequency to first position
+%
+% For even N,
+%   kcirc = [0, 1, ... , N/2, -N/2 + 1, -N/2 + 2, ... , -1]
+%
+% For odd N,
+%   kcirc = [0, 1, ... , (N - 1)/2, -(N - 1)/2, -(N - 1)/2 + 1, ..., -1]
+%
+kcirc = circshift((1:N) - ceil(N/2), floor(N/2)+1);
 
-f = kcyclic*fs;
+f = kcirc*fs;
 f = f(:);
     
 end

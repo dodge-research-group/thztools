a45d46fb1ac0fdbd4a309ec2021bbe9729c2fe10
Steve Dodge <jsdodge@sfu.ca>
2020-10-16 10:29:37 -0700

    Add functions to test fits to Y = diff(X, 1, 2). The results were worse than with X. Also, add test data from Pawitan's _In All Likelihood_, along with a function stub to evaluate different likelihood functions.

---
+++ test/testpdf.m
@@ -0,0 +1,6 @@
+function [logpdf,glogpdf] = testpdf(X,Mu,Sigma)
+%TESTPDF Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end

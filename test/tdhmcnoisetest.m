63d5dafd0cc327a4a15cb18102f6db227be18e39
Steve Dodge <jsdodge@sfu.ca>
2020-10-27 15:05:19 -0700

    Final version of TDHMCNOISETEST for one simulated data set; publish

--- test/tdhmcnoisetest.m
+++ test/tdhmcnoisetest.m
@@ -24,6 +24,8 @@ Noise.time = 10*T/SNR;
 sigma_alpha = Noise.add;
 sigma_beta = Noise.mult;
 sigma_tau = Noise.time;
+
+rng('default')
 epsilon_alpha = sigma_alpha*randn(N,M);
 epsilon_beta = sigma_beta*randn(N,M);
 epsilon_tau = sigma_tau*randn(N,M);
@@ -42,17 +44,17 @@ xn = x + epsilon_alpha + epsilon_beta.*abs(x);
 
 %% Define prior distribution
 LogVAlphaPriorMean = -15;
-LogVAlphaPriorSigma = 5;
+LogVAlphaPriorSigma = 15;
 LogVBetaPriorMean = -15;
-LogVBetaPriorSigma = 5;
+LogVBetaPriorSigma = 15;
 LogVTauPriorMean = -15;
-LogVTauPriorSigma = 5;
+LogVTauPriorSigma = 15;
 MuPriorMean = mean(xn, 2);
-MuPriorSigma = std(xn, 0, 2);
+MuPriorSigma = 10*std(xn, 0, 2);
 LogAPriorMean = zeros(M-1,1);
-LogAPriorSigma = 0.5*ones(M-1,1);
+LogAPriorSigma = 5*ones(M-1,1);
 EtaPriorMean = zeros(M-1,1);
-EtaPriorSigma = 0.5*T*ones(M-1,1);
+EtaPriorSigma = 5*T*ones(M-1,1);
 
 %% Define log posterior
 logpdf = @(P) tdlogposterior(P, xn, T, ...
@@ -64,21 +66,16 @@ logpdf = @(P) tdlogposterior(P, xn, T, ...
     EtaPriorMean, EtaPriorSigma);
 
 %% Create HMC sampler
-logSigmaEst = log(mean(std(xn, 0, 2)));
-muEst = MuPriorMean;
-% startpoint = [logSigmaEst*ones(3,1); muEst; zeros(2*(M-1),1)];
+
 startpoint = [2*log([sigma_alpha; sigma_beta; sigma_tau]); ...
-    xfun(t, tc, w); zeros(2*M-2,1)];
-smp = hmcSampler(logpdf, startpoint, ...
-    'StepSize', 1e-2, ...
-    'NumSteps', 50, ...
-    'CheckGradient', false);
+    xfun(t, tc, w); log(A(2:end)); eta(2:end)];
+smp = hmcSampler(logpdf, startpoint, 'CheckGradient', false);
 
 %% Estimate MAP point
 
 [MAPpars,fitInfo] = estimateMAP(smp,...
     'VerbosityLevel', 0, ...
-    'IterationLimit', 100000);
+    'IterationLimit', 200000);
 MAPlogVarAlpha = MAPpars(1);
 MAPlogVarBeta = MAPpars(2);
 MAPlogVarTau = MAPpars(3);
@@ -92,10 +89,14 @@ ylabel('Negative log density');
 
 %% Tune sampler
 
-[smp,tuneinfo] = tuneSampler(smp, 'Start', MAPpars,...
-    'MassVectorTuningMethod','hessian',...
-    'VerbosityLevel',1,'NumPrint',10,...
-    'NumStepSizeTuningIterations',100);
+[smp,tuneinfo] = tuneSampler(smp, ...
+    'Start', MAPpars, ...
+    'MassVectorTuningMethod', 'hessian', ...
+    'VerbosityLevel', 1, ...
+    'NumPrint', 10);
+
+disp(smp)
+disp('')
 
 figure;
 plot(tuneinfo.StepSizeTuningInfo.StepSizeProfile);
@@ -108,24 +109,39 @@ fprintf("accratio = %.4f\n", tuneinfo.StepSizeTuningInfo.AcceptanceRatio)
 
 NumChains = 4;
 chains = cell(NumChains,1);
-Burnin = 2000;
+Burnin = 500;
 NumSamples = 1000;
+ThinSize = 1;
 for c = 1:NumChains
     if (c == 1)
         level = 1;
     else
         level = 0;
     end
-    chains{c} = drawSamples(smp,'Start',MAPpars + randn(size(MAPpars)), ...
-        'Burnin',Burnin,'NumSamples',NumSamples, ...
-        'VerbosityLevel',level,'NumPrint',100);
+    chains{c} = drawSamples(smp, ...
+        'Start', MAPpars, ...
+        'Burnin', Burnin, ...
+        'NumSamples', NumSamples, ...
+        'ThinSize', ThinSize, ...
+        'VerbosityLevel', level, ...
+        'NumPrint', 100);
 end
 
 %% Examine convergence diagnostics
 
-% diags = diagnostics(smp,chains);
-% disp(diags)
-% disp(2*log([sigma_alpha, sigma_beta, sigma_tau]))
+diags = diagnostics(smp,chains);
+disp(diags)
+
+concatenatedSamples = vertcat(chains{:});
+true_sigma = [sigma_alpha, sigma_beta, sigma_tau];
+mean_exp = sqrt(mean(exp(concatenatedSamples(:,1:3))));
+exp_mean = sqrt(exp(mean(concatenatedSamples(:,1:3))));
+sigmaEst = [true_sigma; mean_exp; exp_mean]';
+
+fprintf("%-15s%15s%15s%15s\n", "Parameter", "True", "<exp(logv)>", "exp(<logv>)")
+fprintf("%-15s%15.4e%15.4e%15.4e\n", "sigma_alpha", sigmaEst(1,:))
+fprintf("%-15s%15.4e%15.4e%15.4e\n", "sigma_beta", sigmaEst(2,:))
+fprintf("%-15s%15.4e%15.4e%15.4e\n", "sigma_tau", sigmaEst(3,:))
 
 %% Visualize samples
 
@@ -141,3 +157,65 @@ figure;
 plot(chains{1}(:,3))
 title("log(\sigma_{\tau}^2), Chain 1")
 
+figure('Name','sigma-squared (alpha)');
+histogram(exp(concatenatedSamples(:,1)))
+xline(sigma_alpha^2,'r-','LineWidth',1)
+xline(mean(exp(concatenatedSamples(:,1))),'k-','LineWidth',1)
+xlabel("\sigma_\alpha^2")
+ylabel("Frequency")
+
+figure('Name','sigma-squared (beta)');
+histogram(exp(concatenatedSamples(:,2)))
+xline(sigma_beta^2,'r-','LineWidth',1)
+xline(mean(exp(concatenatedSamples(:,2))),'k-','LineWidth',1)
+xlabel("\sigma_\beta^2")
+ylabel("Frequency")
+
+figure('Name','sigma-squared (tau)');
+histogram(exp(concatenatedSamples(:,3)))
+xline(sigma_tau^2,'r-','LineWidth',1)
+xline(mean(exp(concatenatedSamples(:,3))),'k-','LineWidth',1)
+xlabel("\sigma_\tau^2")
+ylabel("Frequency")
+
+figure('Name','logA');
+ax = gobjects(M-1,1);
+for i = 1:M-1
+    ax(i) = subplot(3,3,i);
+    histogram(concatenatedSamples(:,3+N+i))
+    xline(log(A(i+1)),'r-','LineWidth',1)
+    xline(mean(concatenatedSamples(:,3+N+i)),'k-','LineWidth',1)
+    xlim(log(A(i+1))+[-.01 .01]);
+    if i == 8
+        ax(i).XTick = log(A(i+1))+[-.01 0 .01];
+        ax(i).XTickLabel = {'-0.001', '0', '0.001'};
+        xlabel(ax(i),'Difference from true log(A_k)');
+    else
+        ax(i).XTickLabel = {};
+    end
+    ax(i).YTickLabel = {};
+    if i==2
+        title("log(A_k)")
+    end
+end
+
+figure('Name','eta');
+ax = gobjects(M-1,1);
+for i = 1:M-1
+    ax(i) = subplot(3,3,i);
+    histogram(concatenatedSamples(:,3+N+M-1+i))
+    xline(eta(i+1),'r-','LineWidth',1)
+    xline(mean(concatenatedSamples(:,3+N+M-1+i)),'k-','LineWidth',1)
+    xlim(eta(i+1)+[-.001 .001]);
+    if i == 8
+        ax(i).XTick = eta(i+1)+[-.001 0 .001];
+        ax(i).XTickLabel = {'-0.001', '0', '0.001'};
+        xlabel(ax(i),'Difference from true \eta_k');
+    else
+        ax(i).XTickLabel = {};
+    end
+    ax(i).YTickLabel = {};
+    if i==2
+        title("\eta_k")
+    end
+end

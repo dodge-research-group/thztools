8b00e6670461fcf177ab810d7fb2016008de7593
Randon Hall <rph4@sfu.ca>
2022-06-09 14:22:46 -0700

    finished shiftmtx.py with comments

---
+++ thztoolsPY/shiftmtx.py
@@ -0,0 +1,27 @@
+import numpy as np
+from scipy import linalg
+from thztools.thztoolsPY.fftfreq import fftfreq
+
+def shiftmtx(tau, n, ts):
+    """
+    shiftmtx computes the n by n transfer matrix for a continuous time-shift
+
+        parameters:
+            tau: Input parameters for the function
+            n: Number of time samples
+            ts: sampling time
+
+        returns:
+            H: Transfer matrix with size (n,n)
+    """
+
+    # Fourier method
+    f = fftfreq(n, ts)
+    w = 2*np.pi*f
+
+    imp = np.fft.ifft(np.exp(-1j*w*tau)).real
+
+    # computes the n by n transformation matrix
+    h = linalg.toeplitz(imp, np.roll(imp[::-1], 1))
+
+    return h

f4788e3805f75dad26188bae7165c1c8f7b8d29a
jonathan Posada Loaiza <jpa166@sfu.ca>
2022-06-10 09:49:09 -0700

    modified test_pulsegen.py; modified test_thzgen.py

---
+++ testsPY/test_pulsegen.py
@@ -0,0 +1,26 @@
+import numpy as np
+import h5py
+
+with h5py.File('pulsegen_test_data.mat', 'r') as file:
+    Set = file['Set']
+    xN = file['Set']['pulsegen']['N'][0]
+    xT0 = file['Set']['pulsegen']['t0'][0]
+    xw = file['Set']['pulsegen']['w'][0]
+    xA = file['Set']['pulsegen']['A'][0]
+    xT = file['Set']['pulsegen']['T'][0]
+    dfN= np.array(xN);  dfT0 = np.array(xT0); dfw = np.array(xw);  dfA = np.array(xA); dfT = np.array(xT)
+
+
+    for i in range(0, df.shape[0]):
+        for j in range(0, df.shape[0]):
+            for k in range(0, df.shape[0]):
+              for r in range(0, df.shape[0]):
+                for s in range(0, df.shape[0]):
+                  N =  np.array(file[Set['pulsegen']['N'][i, j, k, r, s]])[0, 0]
+                  t0 =  np.array(file[Set['pulsegen']['t0'][i, j, k, r, s]])[0, 0]
+                  w =  np.array(file[Set['pulsegen']['w'][i, j, k, r, s]])[0, 0]
+                  A =  np.array(file[Set['pulsegen']['A'][i, j, k, r, s]])[0, 0]
+                  T =  np.array(file[Set['pulsegen']['T'][i, j, k, r, s]])[0, 0]
+                  y =  np.array(file[Set['pulsegen']['y'][i, j, k, r, s]])[0]
+                  fpy = pulsegen(N.astype(int), t0, w  , A, T)[0]
+                  np.testing.assert_allclose(y, fpy)
--- testsPY/test_thzgen.py
+++ testsPY/test_thzgen.py
@@ -1,13 +1,18 @@
-import numpy as np
 import h5py
+import numpy as np
 
 from thztools.thztoolsPY.thzgen import thzgen
 
 with h5py.File('thzgen_test_data.mat', 'r') as file:
     Set = file['Set']
-    for i in range(2):
-        for j in range(2):
-            for k in range(2):
+    xN = file['Set']['thzgen']['N'][0]
+    xT = file['Set']['thzgen']['T'][0]
+    xT0 = file['Set']['thzgen']['t0'][0]
+    dfN= np.array(xN); dfT = np.array(xT); dfT0 = np.array(xT0)
+
+    for i in range(0, dfN.shape[0]):
+        for j in range(0, dfT.shape[0]):
+            for k in range(0, dfT0.shape[0]):
                 N =  np.array(file[Set['thzgen']['N'][i,j,k]])[0, 0]
                 T =  np.array(file[Set['thzgen']['T'][i,j,k]])[0, 0]
                 t0 =  np.array(file[Set['thzgen']['t0'][i,j,k]])[0, 0]

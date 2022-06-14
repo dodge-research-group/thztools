a1ebf36a064032ecd52a61048dd98478c0b8b060
jonathan Posada Loaiza <jpa166@sfu.ca>
2022-06-14 11:39:38 -0700

    created pulserelationdelay.py

--- thztoolsPY/pulserelationdelay.py
+++ thztoolsPY/pulserelationdelay.py
@@ -1,29 +1,49 @@
-import math
-
-
-def pulserelationdelay(signalIn, Parms):
-    # N = len(signalIn)
-    N = np.shape(signalIn)[0]
-    c = np.shape(signalIn)[1]
+def pulserelationdelay(signalIn, Parmsa, Parmsb):
+    N = len(signalIn)
     Nmax = math.floor((N - 1) / 2)
-    nTerm = Parms.shape[0]
+    nTerm = len(Parms[0][0])
 
-    if c = 1:
-        signalIn = signalIn[:, 0]
+    w = np.ones(Nmax)
+    for i in range(len(w)):
+      f = (i+1)/N
+      w[i] = 2*np.pi*f
+      s = 1j*w
 
-    else c != 1:
-        print('Warning: Multiple colums in data arrays, operating on first')
-        signalIn = signalIn[:, 0]
 
     # Fourier transform signal
-    signalInft = np.fft.fft(signalIn)
+    signalInFt = np.fft.fft(signalIn)
+    In = signalInFt[2:Nmax+1]
+
+    for i in range(len(Parms[0])):
+      na = len(Parms[0][i])
+    for j in range(len(Parms[1])):
+      nb = len(Parms[1][j])
+
+
+    nab = max(na, nb)
+
+    kVal = np.zeros(nab)
+    for i in range(len(kVal)):
+      kVal[i] = i
+
+    vanderS = np.power(np.tile((np.reshape(w, (len(w), 1))), nab), kVal) # first re shape w, expands it and then exponentiation
+
+    denominator = vanderS@Parms[0][0]
+    numerator = vanderS@Parms[1][0]
+
+
     tFun = np.zeros(Nmax)
 
-    for i in range(nTerm + 1):
-        na = len(Parms[0]) - 1
-        nb = len(Parms[1]) - 1
+    for i in range(len(Parms[2])):
+      tFun = tFun + (numerator/denominator)*np.exp(Parms[2][i]*s)
 
-        nab = np.max(na, nb)
+    out = tFun*In
 
-        for i in range(nab + 1):
-            vanderS = ()
+    if N % 2== 0:
+      signalOut = np.real(np.fft.ifft(np.concatenate((np.array([0]), out, np.array([0]), np.conj(np.flipud(out))))))
+
+    else:
+      signalOut = np.real(np.fft.ifft(np.concatenate((np.array([0]), out, np.conj(np.flipud(out))))))
+
+
+    return signalOut

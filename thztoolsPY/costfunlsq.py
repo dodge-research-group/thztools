645e5c6fe040c24ffad742e3bb0999e19731f9a1
Santiago Higuera Quintero <sha304@sfu.ca>
2022-06-29 12:03:32 -0700

    Edited test_fftfreq.py; Edited costfunlsq.py

--- thztoolsPY/costfunlsq.py
+++ thztoolsPY/costfunlsq.py
@@ -32,9 +32,11 @@ def costfunlsq(fun, theta, xx, yy, sigmax, sigmay, wfft):
 
     Uy = np.zeros((N, N))
     for k in np.arange(N):
-        Uy = Uy + np.real(np.dot(np.roll(Htilde, k), np.conj(np.roll(Htilde, k)).T)) * (sigmax[k] ** 2)
+        a = np.reshape(np.roll(Htilde, k), (N,1))
+        b = np.reshape(np.conj(np.roll(Htilde, k)), (1, N))
+        Uy = Uy + np.real(np.dot(a, b)) * sigmax[k]**2
 
-    W = np.eye(N) / scipy.linalg.sqrtm(Uy + Vy)
+    W = np.dot(np.eye(N), scipy.linalg.inv(scipy.linalg.sqrtm(Uy + Vy)))
     res = np.dot(W, ry)
 
     return res

591a9b4fd08f9c4afb4eff3fdc9065e9a0246126
Randon Hall <rph4@sfu.ca>
2022-07-07 16:52:31 -0700

    added test_jpcrateeq.py

---
+++ testsPY/test_jpcrateeq.py
@@ -0,0 +1,27 @@
+import numpy as np
+import h5py
+
+from thztools.thztoolsPY.jpcrateeq import jpcrateeq
+
+def test():
+    with h5py.File('jpcrateeq_test_data.mat', 'r') as file:
+        set = file['Set']
+        xt = set['jpcrateeq']['t'][0]
+        xtheta = set['jpcrateeq']['theta'][0]
+        # xdndt = set['jpcrateeq']['dndt'][0]
+        # xdvdt = set['jpcrateeq']['dvdt'][0]
+        # xdpdt = set['jpcrateeq']['dpdt'][0]
+        dft = np.array(xt)
+        dftheta = np.array(xtheta)
+
+        for i in range(0, dft.shape[0]):
+            for j in range(0, dftheta.shape[0]):
+                t = np.array(file[set['jpcrateeq']['t'][i, j]])[0]
+                theta = np.array(file[set['jpcrateeq']['theta'][i, j]])[0]
+                dndt = np.array(file[set['jpcrateeq']['dndt'][i, j]])[0]
+                dvdt = np.array(file[set['jpcrateeq']['dvdt'][i, j]])[0]
+                dpdt = np.array(file[set['jpcrateeq']['dpdt'][i, j]])[0]
+                resultPy = jpcrateeq(t, theta)
+                np.testing.assert_allclose(dndt, resultPy[0])
+                np.testing.assert_allclose(dvdt, resultPy[1])
+                np.testing.assert_allclose(dpdt, resultPy[2])

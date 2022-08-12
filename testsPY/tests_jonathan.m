aadb870dd9a82ebfc15079957619d14ed79598da
jonathan Posada Loaiza <jpa166@sfu.ca>
2022-08-12 13:26:29 -0700

    new file tests.m where data is generated to test all the functions in thztoolsPY

--- testsPY/tests_jonathan.m
+++ testsPY/tests_jonathan.m
@@ -1,26 +1,27 @@
-% % %%TDNOISEFIT %%%%%%
+% %%TDNOISEFIT %%%%%% with dataToptica
+
+Data = dataToptica;
+nDir = length(Data);
+
+%Run loop backwards to preallocate structure array upon first iteration
+Data = Data(nDir:-1:1);
+for iDir = length(Data):-1:1
+    t = Data(iDir).t;
+    X = Data(iDir).X;
+    [~, dirName] = fileparts(Data(iDir).Dir);
+    fprintf("%s\n", repmat('-',1,50));
+    fprintf("%s\n", repmat('-',1,50));
+    fprintf('Fit noise model to data in %s\n', dirName)
+    [Output(iDir), Options, P] = noisefitshow(t, X, replace(dirName, '_', ' '));
+end
+
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5
 % 
-% Data = dataToptica;
-% nDir = length(Data);
 % 
-% %Run loop backwards to preallocate structure array upon first iteration
-% Data = Data(nDir:-1:1);
-% for iDir = length(Data):-1:1
-%     t = Data(iDir).t;
-%     X = Data(iDir).X;
-%     [~, dirName] = fileparts(Data(iDir).Dir);
-%     fprintf("%s\n", repmat('-',1,50));
-%     fprintf("%s\n", repmat('-',1,50));
-%     fprintf('Fit noise model to data in %s\n', dirName)
-%     [Output(iDir), Options, P] = noisefitshow(t, X, replace(dirName, '_', ' '));
-% end
+%  tdnoisefit_data = struct('X', X,  't', t, 'Options', Options, 'Output', Output);
+% 
+%  save tdnoisefit_test_data.mat -v7.3 tdnoisefit_data
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5
-
-
-%tdnoisefit_data = struct('X', X,  't', t, 'Options', Options, 'Output', Output);
-
-%save tdnoisefit_test_data.mat -v7.3 tdnoisefit_data
 % vEst = P(end).var;
 % muEst = P(end).mu;
 % AEst = P(end).A;

98d75893d6e45a5808ad56815bb12c6aea251bb8
jonathan Posada Loaiza <jpa166@sfu.ca>
2023-01-17 17:28:59 -0500

    updated costfun.py, costunflsq.py, noiseamp.py, tdtf.py and thzgen.py in the thztools folder

--- thztools/costfun.py
+++ thztools/costfun.py
@@ -1,24 +1,72 @@
-import numpy as np 
+import numpy as np
+
+from thztoolsPY.noisevar import noisevar
+from thztoolsPY.tdtf import tdtf
+
+
 def costfun(fun, mu, theta, xx, yy, sigma_alpha, sigma_beta, sigma_tau, ts):
+    """
+    Computes the maximum likelihood cost function.
+
+    Parameters
+    ----------
+        fun : callable
+            Transfer function, in the form fun(theta,w), -iwt convention.
+
+        mu : ndarray
+            Signal vector of size (n,).
+
+        theta : ndarray
+            Input parameters for the function.
+
+        xx : ndarray
+            Measured input signal.
+
+        yy : ndarray
+            Measured output signal.
+
+        sigma_alpha : float
+            Noise produced by the detection electronics. ($\\ \sigma _{\\alpha} = 10^{-4} $)
+
+        sigma_beta : float
+            Multiplicative noise term produced by laser power fluctuations. ($\\ \sigma _{\\beta} = 10^{-4} $)
+
+        sigma_tau : float
+            Noise produced by fluctuations in the optical path lengths.
+
+        ts : float
+            Sampling time.
+
+    Returns
+    -------
+        k : callable
+            Negative loglikelihood function.
+
+    """
 
     sigma = [sigma_alpha, sigma_beta, sigma_tau]
-    
+
     n = len(xx)
-    H = tdtf(fun, theta, n, ts)
-    
-    psi = H*mu
-    
-    Vmu = np.diag(noisevar(sigma, mu, ts))
-    Vpsi = np.diag(noisevar(sigma, psi, ts))
-    
-    iVmu = np.diag(1/noisevar(sigma, mu,ts))
-    iVpsi = np.diag(1/noisevar(sigma, psi, ts))
-    
-    
-    
-    iVx = diag(1/noisevar(sigma, xx, ts))
-    iVy = diag(1/noisevar(sigma, yy, ts))
-    
-    
-    K = np.log(np.linalg.det(iVx*iVmu)) + np.log(np.linalg.det(iVy*Vpsi)) + (xx-mu)*iVmu*(xx-mu) + (yy-psi)*(iVpsi)*(yy-psi)    
-    
+    h = tdtf(fun, theta, n, ts)
+
+    psi = h*mu
+
+    vmu = np.diag(noisevar(sigma, mu, ts))
+    vpsi = np.diag(noisevar(sigma, psi, ts))
+
+    ivmu = np.diag(1/noisevar(sigma, mu, ts))
+    ivpsi = np.diag(1/noisevar(sigma, psi, ts))
+
+    # compute the inverse covariance matrices for xx and yy
+    ivx = np.diag(1/noisevar(sigma, xx, ts))
+    ivy = np.diag(1/noisevar(sigma, xx, ts))
+
+    # compute the cost function
+    # Note: sigma-mu and sigma-psi both have determinants below the numerical
+    # precision, so we multiply them by the constant matrices isigmaxx and
+    # isigmayy to improve numerical stability
+    k = np.log(np.linalg.det(ivx*vmu)) + np.log(np.linalge.det(ivy*vpsi)) + \
+        (xx - mu).T*ivmu*(xx - mu) + (yy - psi).T*ivpsi*(yy - psi)
+
+    return k
+
--- thztools/tdtf.py
+++ thztools/tdtf.py
@@ -1,64 +1,60 @@
-from scipy.linalg import toeplitz 
 import numpy as np
-theta = .1
-N = 12
-ts = .05
-def tdtf(fun,theta,N,ts):
-    '''
-    
-    TDTF computes the transfer matrix for a given function
-    
-    Inputs:
-    
-        fun     Transfer function, in the form fun(theta,w), -iwt convention
-        theta   Input parameters for the function
-        N       Number of time samples
-        ts      Sampling time
-    
-    Outputs:
-    
-        h transfer matrix with size (N,N)
+from scipy import linalg
+import math
 
-    '''
-    
-    # computing the transfer function over positive frequencies
-    fs = 1/(ts*N)
-    p = np.arange((N-1)/2)
-    fp = fs*p
-    wp = 2*np.pi*fp
-    tfunp = fun(theta,wp)
-    print(wp)
-    
-    '''
-    From the original matlab code
-    
-    if rem(N,2)~=0
-        tfun = [tfunp; conj(flipud(tfunp(2:end)))];
-    else
-        wNy = pi*N*fs;
-        tfun = [tfunp; conj([fun(theta,wNy);flipud(tfunp(2:end))])];
-    end
-    ''' 
-    tpsh = tfunp[1:end]
-    flipt = np.flipud(tpsh)
-    flipc = np.conj(fliptpsh)
-    
-    
-    #  The transfer function is Hermitian, so the frequencies after the transformation 
-    #  are the same magnitude but negative
-    
 
-    if N % 2 != 0:
-        tfun  = np.concatenate(tfunp,flipt)
+def tdtf(fun, theta, n, ts):
+    """
+    Returns the transfer matrix for a given function.
+
+    It computes the n-by-n transfer matrix for the given function fun with
+    input parameter theta. Note that the transfer function should be written
+    using the physicist's -iwt convention, and that it should be in the format fun(theta,w), where theta
+    is a vector of the function parameters. The transfer function must be
+    Hermitian.
+
+    Parameters
+    ----------
+        fun : callable
+            Transfer function, in the form fun(theta,w), -iwt convention.
+
+        theta : ndarray
+            Input parameters for the function.
+
+        n : int
+            Number of time samples.
+
+        ts : float
+            Sampling time.
+
+    Returns
+    -------
+        h : ndarray or matrix
+            Transfer matrix with size (n,n).
+
+    """
+
+    # compute the transfer function over positive frequencies
+    fs = 1 / (ts * n)
+    fp = fs * np.arange(0, math.floor((n - 1) / 2 + 1))
+    wp = 2 * np.pi * fp
+    tfunp = fun(theta, wp)
+
+    # The transfer function is Hermitian, so we evaluate negative frequencies
+    # by taking the complex conjugate of the corresponding positive frequency.
+    # Include the value of the transfer function at the Nyquist frequency for
+    # even n.
+    if n % 2 != 0:
+        tfun = np.concatenate((tfunp, np.conj(np.flipud(tfunp[1:]))))
+
     else:
-        wny = np.pi*N*fs
-        twny = fun(theta,wNy)
-        conjtwnh = np.concatenate(twny,fliptsh)
-        fliptwny = np.concatenate(twny, fliptpsh)
-        
-        tfun = np.concatenate(tfunp, np.conj(fun(theta,wNy)),fliptpsh)
-    
-    imp = np.real(np.fft.ifft(np.conj(tfun)))
-    h = toeplitz(imp, np.roll(np.flipud(imp),1))
+        wny = np.pi * n * fs
+        tfun = np.concatenate((tfunp, np.conj(np.concatenate((fun(theta, wny), np.flipud(tfunp[1:]))))))
 
-tdtf(fun, theta, N , ts)
+    # Evaluate the impulse response by taking the inverse Fourier transform,
+    # taking the complex conjugate first to convert to ... +iwt convention
+
+    imp = np.real(np.fft.ifft(np.conj(tfun)))
+    h = linalg.toeplitz(imp, np.roll(np.flipud(imp), 1))
+
+    return h

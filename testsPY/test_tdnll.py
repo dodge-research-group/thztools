d6a63fbad06a05bb0dceadc2bcfe627e5fbc53ea
jonathan Posada Loaiza <jpa166@sfu.ca>
2023-01-19 19:09:15 -0500

    testing CI with faulty code

--- testsPY/test_tdnll.py
+++ testsPY/test_tdnll.py
@@ -1,7 +1,7 @@
 import numpy as np
 import h5py
+from thztoolsPY.tdnll import tdnll
 
-from thztools.thztoolsPY.tdnll import tdnll
 
 def test():
     with h5py.File('tdnll_test_data.mat', 'r') as file:
@@ -32,3 +32,4 @@ def test():
                     [nllPy, gradnllPy] = tdnll(x, param, varargin)
                     np.testing.assert_allclose(nllPy, nll)
                     np.testing.assert_allclose(gradnllPy, gradnll)
+                    print('this is a test')

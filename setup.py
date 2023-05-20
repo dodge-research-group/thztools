9f5ae800f09bfc42b0ba888f4866ca9203c078d7
jsdodge <4602669+jsdodge@users.noreply.github.com>
2023-05-19 22:38:55 -0700

    Restore original setup.py after build failed on GitHub Actions

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
-setup()
+setup(
+    name='thztools',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Data analysis tools for THz-TDS',
+    author='Steve Dodge',
+    license='MIT',
+)

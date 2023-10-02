97cefbd2867cb35d931c467294afd9908415c994
jsdodge <4602669+jsdodge@users.noreply.github.com>
2023-10-01 18:31:54 -0700

    Remove tol=1e-3; rerun tdnoisefit-dev

--- src/thztools/thztools.py
+++ src/thztools/thztools.py
@@ -643,7 +643,7 @@ def tdnoisefit(
         )
 
     # Minimize cost function with respect to free parameters
-    out = minimize(objective, x0, method="BFGS", jac=True, tol=1e-3,
+    out = minimize(objective, x0, method="BFGS", jac=True,
                    options={"gtol": 1e-3, "disp": True, "return_all": True})
 
     # Parse output
